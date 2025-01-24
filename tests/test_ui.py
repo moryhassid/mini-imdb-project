bcd51e30c2832fa1f45986ef2a822d2ca6f8c955
Mory_Hassid <mory9870@gmail.com>
2025-01-24 17:19:17 +0200

    selenium tests updated

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -32,3 +32,41 @@ def test_click_new_movie(browser):
         WebDriverWait(browser, 10).until(EC.title_contains('New Movie'))
     except Exception as e:
         pytest.fail(f"Failed to click New Movie link: {e}")
+
+
+def test_add_new_movie(browser):
+    browser.get('http://127.0.0.1:5000/')
+
+    try:
+
+        # Pressing on "Add a New Movie"
+        list_of_links = browser.find_elements(By.TAG_NAME, 'a')
+        new_movie_link = next(
+            link for link in list_of_links if 'New' in link.text)
+        new_movie_link.click()
+
+        values_to_insert = {
+            'Title:': 'King Mory',
+            'Director:': 'Hot Director',
+            'Description': 'This is my description',
+            'Year of Release:': 2023,
+            'Actor 1:': 'Silvester',
+            'Actor 2:': 'Sharon Stone',
+            'Actor 3:': 'Pamela Anderson',
+            'Actor 4:': 'Mr Bean'
+        }
+
+        # Find all input elements with type 'text'
+        text_boxes = browser.find_elements(By.XPATH, "//input[@type='text']")
+
+        for text_box in text_boxes:
+            k = text_box.accessible_name
+            text_box.send_keys(values_to_insert[k])
+
+        # Find all <button> elements
+        buttons = browser.find_elements(By.TAG_NAME, "button")
+        if len(buttons) == 1:
+            buttons[0].click()
+
+    except Exception as e:
+        pytest.fail(f"Failed to add new movie: {e}")
